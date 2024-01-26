6063670d3f4192be4ac71403ebfbcc3f99ec1938
Ivy <ivy7661@gmail.com>
2024-01-26 23:46:07 +0800

    build

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueAxios, axios)
+
+app.mount('#app')
