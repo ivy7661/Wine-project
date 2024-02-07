a085f21b6184cf7ca7e0bef02cca5b56d38d3484
Ray Lin <ray102467@gmail.com>
2024-02-07 14:56:17 +0800

    Updates

---
+++ assets/CartView-6LIpwnUz.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,a as s,t as a,o as n}from"./index-TpOZCpU6.js";const r={name:"CartView",components:{},data(){return{title:"購物車"}},mounted(){}},c={class:"row"};function i(_,l,p,d,t,m){return n(),o("div",c,[s("h1",null,a(t.title),1)])}const f=e(r,[["render",i]]);export{f as default};
