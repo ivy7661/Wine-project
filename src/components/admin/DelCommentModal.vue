0fbd2009c3bdbe59545ffd8191ecc977ba582383
Ivy <ivy7661@gmail.com>
2024-02-17 17:03:58 +0800

    評論管理fin

--- src/components/admin/DelCommentModal.vue
+++ src/components/admin/DelCommentModal.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    id="delProductModal"
-    ref="delProductModal"
+    id="delCommentModal"
+    ref="delCommentModal"
     class="modal fade"
     tabindex="-1"
-    aria-labelledby="delProductModalLabel"
+    aria-labelledby="delCommentModalLabel"
     aria-hidden="true"
   >
     <div class="modal-dialog">
       <div class="modal-content border-0">
         <div class="modal-header bg-danger text-white">
-          <h5 id="delProductModalLabel" class="modal-title">
+          <h5 id="delCommentModalLabel" class="modal-title">
             <span>刪除產品</span>
           </h5>
           <button
@@ -21,15 +21,15 @@
           ></button>
         </div>
         <div class="modal-body">
-          是否刪除
-          <strong class="text-danger">{{ tempProduct.chineseName }}</strong>
-          商品(刪除後將無法恢復)。
+          是否刪除ID
+          <strong class="text-danger">{{ tempComment.id }}</strong>
+          評論(刪除後將無法恢復)。
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
             取消
           </button>
-          <button type="button" class="btn btn-danger" @click="delProduct">確認刪除</button>
+          <button type="button" class="btn btn-danger" @click="delComment">確認刪除</button>
         </div>
       </div>
     </div>
@@ -39,22 +39,22 @@
 <script>
 import { Modal } from 'bootstrap';
 export default {
-  props: ['tempProduct', 'delProduct'],
+  props: ['tempComment', 'delComment'],
   data() {
     return {
-      delModalProduct: null
+      ModalCommentDel: null
     };
   },
   methods: {
     openDelModal() {
-      this.delModalProduct.show();
+      this.ModalCommentDel.show();
     },
     closeDelModal() {
-      this.delModalProduct.hide();
+      this.ModalCommentDel.hide();
     }
   },
   mounted() {
-    this.delModalProduct = new Modal(this.$refs.delProductModal);
+    this.ModalCommentDel = new Modal(this.$refs.delCommentModal);
   }
 };
 </script>
--- src/views/admin/CommentsView.vue
+++ src/views/admin/CommentsView.vue
@@ -4,8 +4,8 @@
     <table class="table mt-4">
       <thead>
         <tr>
-          <th width="60">評論ID</th>
-          <th width="60">使用者ID</th>
+          <th width="70">評論ID</th>
+          <th width="70">使用者ID</th>
           <th width="120">評論內容</th>
           <th width="120">詳細資訊</th>
           <th width="120">刪除評論</th>
@@ -15,13 +15,17 @@
         <tr v-for="comment in comments" :key="comment.id">
           <td>{{ comment.id }}</td>
           <td>{{ comment.userId }}</td>
-          <td>{{ comment.comment }}</td>
+          <td>
+            <p class="commentContent">
+              {{ comment.comment }}
+            </p>
+          </td>
           <td>
             <div class="btn-group">
               <button
                 type="button"
                 class="btn btn-outline-primary btn-sm"
-                @click="openModal(comment)"
+                @click="openModal('detail', comment)"
               >
                 查看
               </button>
@@ -32,7 +36,7 @@
               <button
                 type="button"
                 class="btn btn-outline-danger btn-sm"
-                @click="openModal(comment)"
+                @click="openModal('delete', comment)"
               >
                 刪除
               </button>
@@ -42,18 +46,32 @@
       </tbody>
     </table>
   </div>
+  <!-- 詳細評論內容 Modal -->
+  <Comment-Modal :temp-comment="tempComment" ref="commentModal"></Comment-Modal>
+  <!-- 刪除警示 -->
+  <Del-Comment-Modal
+    :temp-comment="tempComment"
+    :del-comment="delComment"
+    ref="delCommentModal"
+  ></Del-Comment-Modal>
 </template>
 
 <script>
 import axios from 'axios';
+import CommentModal from '../../components/admin/CommentModal.vue';
+import DelCommentModal from '../../components/admin/DelCommentModal.vue';
 const { VITE_API_URL } = import.meta.env;
 export default {
   name: 'AdminCommentsView',
-  components: {},
+  components: {
+    CommentModal,
+    DelCommentModal
+  },
   data() {
     return {
       title: '評論管理',
-      comments: []
+      comments: [],
+      tempComment: {}
     };
   },
   mounted() {
@@ -71,9 +89,39 @@ export default {
         .catch(() => {
           alert('獲取評論失敗');
         });
+    },
+    openModal(status, comment) {
+      if (status === 'detail') {
+        this.tempComment = { ...comment };
+        this.$refs.commentModal.openModal();
+      } else if (status === 'delete') {
+        this.tempComment = { ...comment };
+        this.$refs.delCommentModal.openDelModal();
+      }
+    },
+    delComment() {
+      const url = `${VITE_API_URL}/comments/${this.tempComment.id}`;
+
+      axios
+        .delete(url)
+        .then((res) => {
+          alert('刪除成功');
+          this.$refs.delCommentModal.closeDelModal();
+          this.getComments();
+        })
+        .catch(() => {
+          alert('刪除失敗');
+        });
     }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.commentContent {
+  width: 560px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
