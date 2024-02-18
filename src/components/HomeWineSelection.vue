697333647c089b84cd3bb0e123f0f9d183560515
Ray Lin <ray102467@gmail.com>
2024-02-18 17:05:13 +0800

    修正編譯錯誤

--- src/components/HomeWineSelection.vue
+++ src/components/HomeWineSelection.vue
@@ -33,163 +33,130 @@
           </div>
         </div>
       </div>
-
-    </div>
-
-  </div>
-  <div id="productModal">
-    <div class="card-body d-flex flex-column align-items-start">
-      <strong class="d-flex justify-content-between">引導選酒區</strong>
-      <p>{{ process }}</p>
-      <button class="d-flex justify-content-between btn btn-primary">next</button>
-      <button class="d-flex justify-content-between btn btn-primary">back</button>
-      <div class="row">
-        <div class="col-6">
-          <div v-for="item in  ingredientDirection" :key="item.item" v-if="selectedTaste == item.item"><img
-              :src="item.imgUrl" alt="Ingredient Image"></div>
-          <div v-for="item in ingredientBody" :key="item.item" v-if="selectedBody === item.item">
-            <img :src="item.imgUrl" alt="Ingredient Image">
-          </div>
-          <div v-if="selectedBody === '輕盈'">
-            <div v-for="item in ingredientBubble" :key="item.item">
-              <img :src="item.imgUrl" alt="Ingredient Image">
-            </div>
-          </div>
-
-        </div>
-
-      </div>
     </div>
   </div>
 </template>
-
 
 <script>
 export default {
   name: 'HomeWineSelection',
   data() {
     return {
-      process: "",
-      selectedTaste: "",
-      selectedBody: "",
-      selectedMeal: "",
+      process: '',
+      selectedTaste: '',
+      selectedBody: '',
+      selectedMeal: '',
       ingredientDirection: [
         {
-          item: "偏甜",
-          imgUrl: "偏甜.webp",
+          item: '偏甜',
+          imgUrl: '偏甜.webp'
         },
         {
-          item: "偏酸",
-          imgUrl: "偏酸.webp",
+          item: '偏酸',
+          imgUrl: '偏酸.webp'
         },
         {
-          item: "偏乾",
-          imgUrl: "偏乾.webp",
+          item: '偏乾',
+          imgUrl: '偏乾.webp'
         }],
       ingredientBody: [{
-        item: "飽滿",
-        imgUrl: "飽滿.webp",
+        item: '飽滿',
+        imgUrl: '飽滿.webp'
       },
       {
-        item: "輕盈",
-        imgUrl: "輕盈.webp",
-      }]
-      ,
+        item: '輕盈',
+        imgUrl: '輕盈.webp'
+      }],
       ingredientBubble: [{
-        item: "氣泡",
-        imgUrl: "氣泡.webp",
+        item: '氣泡',
+        imgUrl: '氣泡.webp'
       },
       {
-        item: "無氣泡",
-        imgUrl: "無氣泡.webp",
+        item: '無氣泡',
+        imgUrl: '無氣泡.webp'
       }],
 
       ingredientColor: [{
-        item: "白葡萄酒",
-        imgUrl: "白葡萄酒.webp",
+        item: '白葡萄酒',
+        imgUrl: '白葡萄酒.webp'
       },
       {
-        item: "紅葡萄酒",
-        imgUrl: "紅葡萄酒.webp",
-      }]
-      ,
+        item: '紅葡萄酒',
+        imgUrl: '紅葡萄酒.webp'
+      }],
       ingredientMeal: [{
-        item: "開胃菜",
-        imgUrl: "開胃菜.webp",
+        item: '開胃菜',
+        imgUrl: '開胃菜.webp'
       },
       {
-        item: "主菜",
-        imgUrl: "主菜.webp",
+        item: '主菜',
+        imgUrl: '主菜.webp'
       }, {
-        item: "甜點",
-        imgUrl: "甜點.webp",
-      }]
-      , ingredientMain: [{
-        item: "搭配紅肉",
-        imgUrl: "搭配紅肉.webp",
+        item: '甜點',
+        imgUrl: '甜點.webp'
+      }],
+      ingredientMain: [{
+        item: '搭配紅肉',
+        imgUrl: '搭配紅肉.webp'
       },
       {
-        item: "搭配白肉",
-        imgUrl: "搭配白肉.webp",
+        item: '搭配白肉',
+        imgUrl: '搭配白肉.webp'
       },
 
       {
-        item: "搭配辛辣食物",
-        imgUrl: "搭配辛辣食物.webp",
+        item: '搭配辛辣食物',
+        imgUrl: '搭配辛辣食物.webp'
       },
       {
-        item: "搭配海鮮",
-        imgUrl: "搭配海鮮.webp",
+        item: '搭配海鮮',
+        imgUrl: '搭配海鮮.webp'
       }],
       ingredientDessert: [
         {
-          item: "巧克力",
-          imgUrl: "巧克力.webp",
+          item: '巧克力',
+          imgUrl: '巧克力.webp'
         },
         {
-          item: "水果",
-          imgUrl: "水果.webp",
+          item: '水果',
+          imgUrl: '水果.webp'
         }, {
-          item: "奶油蛋糕",
-          imgUrl: "奶油蛋糕.webp",
-        },
+          item: '奶油蛋糕',
+          imgUrl: '奶油蛋糕.webp'
+        }
       ],
       ingredientRest: [{
-        item: "中等酒體",
-        imgUrl: "中等酒體.webp",
+        item: '中等酒體',
+        imgUrl: '中等酒體.webp'
       },
 
       {
-        item: "甜味果香味",
-        imgUrl: "甜味果香味.webp",
+        item: '甜味果香味',
+        imgUrl: '甜味果香味.webp'
       },
       {
-        item: "濃郁果味甜度",
-        imgUrl: "濃郁果味甜度.webp",
+        item: '濃郁果味甜度',
+        imgUrl: '濃郁果味甜度.webp'
       },
       {
-        item: "清新果味",
-        imgUrl: "清新果味.webp",
+        item: '清新果味',
+        imgUrl: '清新果味.webp'
       },
       {
-        item: "輕盈氣泡酒",
-        imgUrl: "輕盈氣泡酒.webp",
+        item: '輕盈氣泡酒',
+        imgUrl: '輕盈氣泡酒.webp'
       }],
 
       ingredientBubbleWine: [{
-        item: "紅葡萄氣泡酒",
-        imgUrl: "紅葡萄氣泡酒.webp",
+        item: '紅葡萄氣泡酒',
+        imgUrl: '紅葡萄氣泡酒.webp'
       },
       {
-        item: "白葡萄氣泡酒",
-        imgUrl: "白葡萄氣泡酒.webp",
+        item: '白葡萄氣泡酒',
+        imgUrl: '白葡萄氣泡酒.webp'
       }]
 
-
-
-
-    }
-
+    };
   },
   methods: {
     mainContain() {
@@ -205,4 +172,5 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
