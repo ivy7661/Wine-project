8944f99ba8dba7035b0c44f1d5b273868c4bed6c
Po Wei <lapo090790@shms-mail.ch>
2024-03-09 21:26:02 +0800

    fix

--- src/views/user/KnowledgeView.vue
+++ src/views/user/KnowledgeView.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="container">
+  <div class="container">
     <!-- Red Grape Varieties Section -->
     <div class="container">
       <div class="row justify-content-center">
@@ -37,22 +37,22 @@
   </div>
 
   <div class="container mt-5">
-      <h2 class="mb-4">醒酒技巧</h2>
-      <p>醒酒是指让葡萄酒与空气接触，促进其释放香气和风味的过程。</p>
-      <div v-for="(technique, index) in decantingTechniques" :key="`technique-${index}`" class="card">
-        <div class="card-header" @click="toggleTechnique(index)">
-          <h2 class="accordion-header">
-            <button class="accordion-button" type="button">
-              {{ technique.title }}
-            </button>
-          </h2>
-        </div>
-        <div v-if="technique.isOpen" class="collapse show">
-          <div class="card-body" v-html="technique.description"></div>
-          <img :src="technique.image" class="w-100 mb-5">
-        </div>
+    <h2 class="mb-4">醒酒技巧</h2>
+    <p>醒酒是指让葡萄酒与空气接触，促进其释放香气和风味的过程。</p>
+    <div v-for="(technique, index) in decantingTechniques" :key="`technique-${index}`" class="card">
+      <div class="card-header" @click="toggleTechnique(index)">
+        <h2 class="accordion-header">
+          <button class="accordion-button" type="button">
+            {{ technique.title }}
+          </button>
+        </h2>
+      </div>
+      <div v-if="technique.isOpen" class="collapse show">
+        <div class="card-body" v-html="technique.description"></div>
+        <img :src="technique.image" class="w-100 mb-5">
       </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
@@ -87,7 +87,6 @@ export default {
               風味特點：梅洛葡萄酒通常呈現出紅果風味，如紅櫻桃、紅李子、草莓和覆盆子，並伴隨著一些香料、巧克力、咖啡和煙燻的氣息。這些葡萄酒的風格可以從輕盈和果香濃郁到濃厚和豐滿的不同風格。
               葡萄酒產區：梅洛葡萄品種在世界各地都有廣泛種植，並且在不同產區產生了多種風格的葡萄酒。波爾多地區的聖愛美倫、波美侯和普雷奧拉克（Pomerol）是梅洛的主要產區，以生產出色的梅洛葡萄酒而聞名。此外，美國的加州（尤其是納帕谷和索諾馬谷）、意大利的威尼托地區、澳大利亞的康寧巴拉和智利的馬切特產區也是梅洛葡萄的主要種植地。
               配餐建議：梅洛葡萄酒通常搭配紅肉、家禽、豬肉、羊肉和各種乳製品非常出色。它的柔滑口感和豐富的風味使其成為許多美食的理想伴侶。`
-     
         },
         {
           name: '卡本內蘇維濃',
@@ -107,11 +106,10 @@ export default {
               葡萄酒有多种风格，包括新鲜、果味浓郁的博若莱新酒（Beaujolais Nouveau）和更为复杂、陈年潜力更大的博若莱村庄酒（Beaujolais-Villages）和十大特级园酒（Beaujolais
               Crus）。
               这种葡萄在全球范围内也有种植，但以博若莱地区的版本最为知名。由于其风味轻盈和酸度良好，Gamay 葡萄酒特别适合搭配各种食物，包括肉类、奶酪和各种小吃。`
-     
         }
       ],
       whiteGrapeVarieties: [
-      {
+        {
           name: '雷司令',
           image: '/images/knowledge/riesling.png',
           description: `起源：雷司令葡萄品種起源於德國，特別是來自萊茵蘭-普法茲（Rheingau）和摩澤爾（Mosel）等地區。它也在法國的阿爾薩斯（Alsace）地區、奧地利、澳大利亞、美國、紐西蘭等葡萄酒產區廣泛種植。
@@ -119,8 +117,8 @@ export default {
               風味特點：雷司令葡萄酒具有多種風味，取決於產地和釀造風格。它們通常呈現蘋果、橙子、柚子、桃子等各種柑橘和石果風味，伴隨著蜂蜜、花香、礦物質和石墨的氣息。雷司令葡萄酒的風格可以從非常乾燥和清新到甜度較高的冰酒，因此適合各種口味的人。
               葡萄酒產區：雷司令葡萄品種在全球各地都有廣泛種植，但以德國和阿爾薩斯最為著名。德國的莱茵和摩澤爾地區以出產優質的雷司令葡萄酒而聞名，包括多種不同甜度的葡萄酒。阿爾薩斯地區的雷司令葡萄酒通常是乾型風格，具有濃郁的風味。此外，美國的華盛頓州和俄勒岡州也有出色的雷司令葡萄酒。
               配餐建議：雷司令葡萄酒搭配食物非常靈活，可以搭配各種菜餚，從海鮮和家禽到辛辣食物和亞洲美食都適合。此外，它也是一種出色的小吃酒，可以單獨享用。`
-        },      
-         {
+        },
+        {
           name: '白蘇維濃',
           image: '/images/knowledge/Sauvignon blanc.png',
           description: `起源：白蘇維濃葡萄品種起源於法國，最早可以追溯到波爾多地區。它是卡本內蘇維濃（Cabernet Sauvignon）和蘇維濃（Sauvignon
@@ -129,7 +127,7 @@ export default {
               風味特點：白蘇維濃葡萄酒通常呈現出青草、青椒、柑橘類水果（如青橙和橙皮）以及新鮮的草莓、木瓜和梨等風味。它們的氣味濃郁，帶有一絲礦物質和草本的氣息，並具有清爽、為口感的特點。
               葡萄酒產區：白蘇維濃葡萄品種在全球各地都有廣泛種植，並且在不同的產區中表現出不同的風格。最著名的產區之一是法國的卡多內（Loire）地區，特別是來自蘋果卡多內（Sancerre）和普依-芒塔布（Pouilly-Fumé）的白蘇維濃葡萄酒。此外，紐西蘭（特別是馬爾伯勒地區）、美國（尤其是加州和華盛頓州）、智利和南非也生產優質的白蘇維濃葡萄酒。
               配餐建議：白蘇維濃葡萄酒搭配海鮮、沙拉、白肉、蔬菜和各種奶酪非常出色。它們的酸度和清新口感使其成為許多不同風味的菜餚的絕佳伴侶。`
-     
+
         },
         {
           name: '夏多麗',
@@ -140,73 +138,73 @@ export default {
               風味特點：夏多麗葡萄酒的風味特點因產地和釀造方法的不同而變化很大。它們通常呈現綠蘋果、柚子、梨子、柑橘、菠蘿等水果風味，並伴隨著奶油、香草、堅果和礦物質的氣息。夏多麗葡萄酒可以是清新的、酸度高的，也可以是富有濃度、圓潤和帶有橡木桶風味的複雜型白葡萄酒。
               葡萄酒產區：夏多麗葡萄品種在全球各地都有廣泛種植，並且被廣泛用於各種白葡萄酒的釀造。法國的勃艮第地區、勃艮第的莫爾東、香檳、波爾多地區的白海岸（白貝爾格）以及新世界的加州、澳大利亞、紐西蘭、南非等地都以生產出色的夏多麗葡萄酒而聞名。
               配餐建議：夏多麗葡萄酒非常適合搭配各種食物，包括家禽、魚類、海鮮、蔬菜、奶酪和各種沙拉。它的風味多樣性和酸度使其成為許多菜餚的理想伴侶。`
-     
+
         }
       ],
       openers: [
         {
-          title: "螺旋钻式开瓶器（侍酒师之友）",
+          title: '螺旋钻式开瓶器（侍酒师之友）',
           description: `将开瓶器的尖端对准软木塞的中心。<br>
               旋转手柄，将螺旋钻缓慢旋入软木塞中。<br>
               当螺旋钻几乎完全进入软木塞时停止。<br>
               使用开瓶器的杠杆部分抵住瓶颈边缘，轻轻拉起手柄，将软木塞从瓶子中取出。`,
-          image: "/images/knowledge/opener1.png",
+          image: '/images/knowledge/opener1.png',
           isOpen: false
         },
         {
-          title: "翼式开瓶器",
+          title: '翼式开瓶器',
           description: `将开瓶器的中心针对准软木塞中心。<br>
               旋转顶部的手柄，使螺旋钻进入软木塞。<br>
               继续旋转直到两边的“翼”完全上升。<br>
               按下两边的“翼”，以此力量将软木塞从瓶中拔出。`,
-          image: "/images/knowledge/opener2.png",
+          image: '/images/knowledge/opener2.png',
           isOpen: false
         },
         {
-          title: "电动开瓶器",
+          title: '电动开瓶器',
           description: `将电动开瓶器的开口对准软木塞。<br>
               启动电动机，让其自动将螺旋钻旋入软木塞中。<br>
               当开瓶器停止旋转时，说明软木塞已被完全拔出。<br>
               可能需要按下另一个按钮来释放软木塞。`,
-          image: "/images/knowledge/opener3.png",
+          image: '/images/knowledge/opener3.png',
           isOpen: false
         }
-     
+
       ],
       decantingTechniques: [
         {
-          title: "使用醒酒器（Decanter）",
+          title: '使用醒酒器（Decanter）',
           description: `<strong>操作方法：</strong>将葡萄酒缓缓倒入醒酒器中。醒酒器通常有宽阔的底部，可以增加葡萄酒与空气的接触面积。<br>
           <strong>适用类型：</strong>适用于年份较老的红葡萄酒，或者需要释放更多香气和风味的年轻、单宁较重的葡萄酒。`,
-          image: "/images/knowledge/glass1.png",
+          image: '/images/knowledge/glass1.png',
           isOpen: false
         },
         {
-          title: "直接在酒杯中醒酒",
+          title: '直接在酒杯中醒酒',
           description: `<strong>操作方法：</strong>将葡萄酒倒入宽口酒杯中，让它在杯中摇晃几分钟。<br>
           <strong>适用类型：</strong>适用于不需要长时间醒酒的葡萄酒，或当你没有醒酒器时的临时解决方案。`,
-          image: "/images/knowledge/glass2.png",
+          image: '/images/knowledge/glass2.png',
           isOpen: false
         },
         {
-          title: "旋转酒杯",
+          title: '旋转酒杯',
           description: `<strong>操作方法：</strong>倒入少量葡萄酒后，轻轻旋转酒杯，让葡萄酒沿杯壁转动，这样可以增加葡萄酒与空气的接触。<br>
           <strong>适用类型：</strong>几乎适用于所有类型的葡萄酒，特别是在品酒时。`,
-          image: "/images/knowledge/glass3.png",
+          image: '/images/knowledge/glass3.png',
           isOpen: false
         },
         {
-          title: "  使用醒酒塞或醒酒漏斗",
+          title: '使用醒酒塞或醒酒漏斗',
           description: `<strong>操作方法：</strong>这些工具可以在倒酒时帮助葡萄酒与空气混合。醒酒塞或漏斗安装在瓶口，当酒倒出时自然引入空气。<br>
           <strong>适用类型：</strong>适合需要稍微醒酒的年轻红葡萄酒和某些白葡萄酒。`,
-          image: "/images/knowledge/glass4.png",
+          image: '/images/knowledge/glass4.png',
           isOpen: false
         },
         {
-          title: "  提前开瓶",
+          title: '提前开瓶',
           description: `<strong>操作方法：</strong>提前几小时打开葡萄酒瓶，让酒瓶口与空气接触。<br>
           <strong>适用类型：</strong>适用于大多数红葡萄酒，尤其是单宁较重的品种。`,
-          image: "/images/knowledge/glass5.png",
+          image: '/images/knowledge/glass5.png',
           isOpen: false
         }
       ]
