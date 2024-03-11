d1d9ac9ccbc6be2cd1bbe90c61bb30894d4d5ea5
Ray Lin <ray102467@gmail.com>
2024-03-11 16:52:08 +0800

    調整 vite config 的 .env 值判斷

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -12,31 +12,34 @@ const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
 const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
-  plugins: [
-    vue(),
-    eslintPlugin({
-      include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      output: {
-        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
-        sanitizeFileName(name) {
-          const match = DRIVE_LETTER_REGEX.exec(name);
-          const driveLetter = match ? match[0] : '';
-          // substr 是被淘汰語法，因此要改 slice
-          return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '');
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: mode === 'production' ? `/${env.REPOSITORY_NAME}/` : '/',
+    plugins: [
+      vue(),
+      eslintPlugin({
+        include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      chunkSizeWarningLimit: 1500,
+      rollupOptions: {
+        output: {
+          // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+          sanitizeFileName(name) {
+            const match = DRIVE_LETTER_REGEX.exec(name);
+            const driveLetter = match ? match[0] : '';
+            // substr 是被淘汰語法，因此要改 slice
+            return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '');
+          }
         }
       }
     }
-  }
+  };
 });
