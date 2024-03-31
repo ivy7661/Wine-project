8157a92075389ec22a76ae092359b8cc3863f298
Ray Lin <ray102467@gmail.com>
2024-03-31 22:14:47 +0800

    Updates

---
+++ assets/KnowledgeView-rXR5A0Fp.js
@@ -0,0 +1,51 @@
+import{_ as u,c as o,a as e,F as l,j as d,o as t,y as a,t as g,i as p,p as h,e as v}from"./index-KDcbRjT1.js";const b={name:"KnowledgeView",data(){return{redGrapeVarieties:[{name:"希拉",image:"/images/knowledge/syrah.png",description:`起源：希拉葡萄品種最初起源於法國的羅納河谷，並在整個法國多個葡萄酒產區中廣泛種植，尤其是在隆河谷地區。然而，它也在全球範圍內廣泛種植，包括澳大利亞、南非、美國、智利、阿根廷等地。
+葡萄特性：希拉葡萄是黑皮葡萄，通常呈現深紫色至黑色的果實。葡萄叢小而緊湊，葡萄粒小，皮薄含有豐富的色素和單寧。
+風味特點：希拉葡萄釀造的葡萄酒通常具有豐富的水果風味，包括黑莓、覆盆子、李子和黑櫻桃等。此外，它還可能伴隨著辛香和香料的味道，如胡椒、肉桂和丁香。希拉葡萄酒的風格因產地、氣候和釀酒師的不同而有所差異，可以是濃烈的、濃鬱的，也可以是較輕盈和優雅的。
+葡萄酒產區：希拉葡萄品種在全球各地廣泛種植，並產生不同風格的葡萄酒。法國的隆河谷產區（如希拉和康涅），澳大利亞的巴羅莎穀和麥克拉倫穀，以及美國的加州，尤其是納帕谷和索諾馬穀，都以出產高質量的希拉葡萄酒而聞名。
+
+配餐建議:希拉葡萄酒通常搭配紅肉、燒烤、燉菜、起司和巧克力`},{name:"黑皮諾",image:"/images/knowledge/pinot noir.png",description:`起源：黑皮諾葡萄品種起源於法國，特別是勃艮第地區，被認為是世界上最古老的葡萄品種之一。它也在其他法國地區（如阿爾薩斯）、德國、義大利、美國（特別是加州和俄勒岡）、紐西蘭等地區廣泛種植。
+              葡萄特性：黑皮諾葡萄通常呈現小而緊湊的葡萄叢，葡萄粒小，皮薄，色素相對少。這使得黑皮諾葡萄容易受到氣候和土壤的影響，因此它是一種具有挑戰性的品種，需要特別的照顧和釀酒技術。
+              風味特點：黑皮諾葡萄酒通常呈現出紅果風味，如紅櫻桃、草莓和覆盆子，以及花香和香料的氣息。它們的風味可以非常優雅和複雜，具有柔滑的口感和悅耳的酸度。由於其敏感性，不同產區和釀酒師的手法可以產生多種風格的黑皮諾葡萄酒。
+              葡萄酒產區：黑皮諾在全球各地都有廣泛種植，但最著名的產區之一是法國的勃艮第地區，特別是布爾岡尼（Bourgogne）地區，產出了一些世界上最受歡迎和尊貴的黑皮諾葡萄酒。此外，美國的加州（尤其是納帕谷和索諾馬穀）也以生產出色的黑皮諾葡萄酒而聞名。
+              配餐建議：黑皮諾葡萄酒通常搭配家禽、魚、豬肉、野味和各種乳製品（如乳酪）非常適合。它的輕盈`},{name:"梅洛",image:"/images/knowledge/merlot.png",description:`起源：梅洛葡萄品種最早起源於法國波爾多地區，特別是聖愛美倫（Saint-Émilion）和波美侯（Pomerol）等產區。它也在全球各地廣泛種植，包括法國、義大利、美國、智利、阿根廷、澳大利亞和南非等葡萄酒產區。
+              葡萄特性：梅洛葡萄通常呈現中等大小的葡萄粒，皮薄，呈現深紅色至紫色的果實。這個品種的葡萄粒具有柔軟的單寧和悅耳的酸度，使其成為一種容易釀造的葡萄品種。
+              風味特點：梅洛葡萄酒通常呈現出紅果風味，如紅櫻桃、紅李子、草莓和覆盆子，並伴隨著一些香料、巧克力、咖啡和煙燻的氣息。這些葡萄酒的風格可以從輕盈和果香濃鬱到濃厚和豐滿的不同風格。
+              葡萄酒產區：梅洛葡萄品種在世界各地都有廣泛種植，並且在不同產區產生了多種風格的葡萄酒。波爾多地區的聖愛美倫、波美侯和普雷奧拉克（Pomerol）是梅洛的主要產區，以生產出色的梅洛葡萄酒而聞名。此外，美國的加州（尤其是納帕谷和索諾馬穀）、義大利的威尼托地區、澳大利亞的康寧巴拉和智利的馬切特產區也是梅洛葡萄的主要種植地。
+              配餐建議：梅洛葡萄酒通常搭配紅肉、家禽、豬肉、羊肉和各種乳製品非常出色。它的柔滑口感和豐富的風味使其成為許多美食的理想伴侶。`},{name:"卡本內蘇維濃",image:"/images/knowledge/cabernet sauvignon.png",description:`起源：卡本內蘇維濃葡萄品種最初起源於法國波爾多地區，是卡本內佈朗（Cabernet Franc）和蘇維濃（Sauvignon
+              Blanc）兩個葡萄品種的雜交品種。它在波爾多地區的聖愛美倫（Saint-Émilion）、帕烏克拉特-梅德克（Pauillac-Médoc）和格拉夫（Graves）等地區廣泛種植。
+              葡萄特性：卡本內蘇維濃葡萄通常呈現中等大小的葡萄粒，皮薄但具有濃厚的色素和單寧。這種葡萄品種對氣候和土壤具有很高的適應性，可以在不同的葡萄酒產區中生長和釀造。
+              風味特點：卡本內蘇維濃葡萄酒通常呈現出黑色水果風味，如黑加侖子、黑莓和黑櫻桃，伴隨著香料、煙草、咖啡和革的氣息。這些葡萄酒具有豐滿的口感、濃鬱的結構和良好的潛力，可以經過陳年讓風味更加複雜和柔和。
+              葡萄酒產區：卡本內蘇維濃葡萄品種在全球各地都有廣泛種植，但法國波爾多地區仍然是其最著名的產區之一。波爾多的梅德克（Médoc）地區、美國的加州（特別是納帕谷和索諾馬谷）、智利、阿根廷、澳大利亞的康寧巴拉等地也以出產優質的卡本內蘇維濃葡萄酒而聞名。
+              配餐建議：卡本內蘇維濃葡萄酒通常搭配紅肉、羊肉、牛肉、燒烤和各種乳酪非常出色。由於其豐滿的結構和複雜的風味，它也可以單獨享用或與深色巧克力搭配。`},{name:"佳美",image:"/images/knowledge/gamay.png",description:`葡萄是一種主要用於釀造紅葡萄酒的葡萄品種，最著名的產區位於法國的博若萊（Beaujolais）地區。這種葡萄品種產生的葡萄酒通常具有濃郁的果香、鮮明的酸度和較低的單寧含量，使得這些葡萄酒易於飲用且適合年輕飲用。
+              Gamay 葡萄的特點是成熟早、顏色淺、酒體輕盈。這些葡萄酒常常展現出紅色水果的味道，如草莓、櫻桃和覆盆子，有時還會有一點香料或花卉的味道。在博若萊地區，Gamay
+              葡萄酒有多種風格，包括新鮮、果味濃郁的博若萊新酒（Beaujolais Nouveau）和更為複雜、陳年潛力更大的博若萊村莊酒（Beaujolais-Villages）和十大特級園酒（Beaujolais
+              Crus）。
+              這種葡萄在全球範圍內也有種植，但以博若萊地區的版本最為知名。由於其風味輕盈和酸度良好，Gamay 葡萄酒特別適合搭配各種食物，包括肉類、乳酪和各種小吃。`}],whiteGrapeVarieties:[{name:"雷司令",image:"/images/knowledge/riesling.png",description:`起源：雷司令葡萄品種起源於德國，特別是來自萊茵蘭-普法茲（Rheingau）和摩澤爾（Mosel）等地區。它也在法國的阿爾薩斯（Alsace）地區、奧地利、澳大利亞、美國、紐西蘭等葡萄酒產區廣泛種植。
+              葡萄特性：雷司令葡萄通常呈現小而緊湊的葡萄叢，葡萄粒小，皮薄，具有高酸度。這使得雷司令葡萄成熟後可以保持其新鮮和清新的風味。
+              風味特點：雷司令葡萄酒具有多種風味，取決於產地和釀造風格。它們通常呈現蘋果、柳丁、柚子、桃子等各種柑橘和石果風味，伴隨著蜂蜜、花香、礦物質和石墨的氣息。雷司令葡萄酒的風格可以從非常乾燥和清新到甜度較高的冰酒，因此適合各種口味的人。
+              葡萄酒產區：雷司令葡萄品種在全球各地都有廣泛種植，但以德國和阿爾薩斯最為著名。德國的萊茵和摩澤爾地區以出產優質的雷司令葡萄酒而聞名，包括多種不同甜度的葡萄酒。阿爾薩斯地區的雷司令葡萄酒通常是乾型風格，具有濃鬱的風味。此外，美國的華盛頓州和俄勒岡州也有出色的雷司令葡萄酒。
+              配餐建議：雷司令葡萄酒搭配食物非常靈活，可以搭配各種菜餚，從海鮮和家禽到辛辣食物和亞洲美食都適合。此外，它也是一種出色的小吃酒，可以單獨享用。`},{name:"白蘇維濃",image:"/images/knowledge/Sauvignon blanc.png",description:`起源：白蘇維濃葡萄品種起源於法國，最早可以追溯到波爾多地區。它是卡本內蘇維濃（Cabernet Sauvignon）和蘇維濃（Sauvignon
+              Blanc）兩個葡萄品種的家族成員。蘇維濃（Sauvignon）一詞意為"野生"，因為這種葡萄品種的野生品種在法國野外生長得相當廣泛。
+              葡萄特性：白蘇維濃葡萄通常呈現中等大小的葡萄粒，皮薄並呈現淡綠色至淡黃色的果實。這種葡萄品種通常具有高酸度和悅耳的芳香，這使得它們成為釀造清新的白葡萄酒的理想選擇。
+              風味特點：白蘇維濃葡萄酒通常呈現出青草、青椒、柑橘類水果（如青橙和橙皮）以及新鮮的草莓、木瓜和梨等風味。它們的氣味濃鬱，帶有一絲礦物質和草本的氣息，並具有清爽、為口感的特點。
+              葡萄酒產區：白蘇維濃葡萄品種在全球各地都有廣泛種植，並且在不同的產區中表現出不同的風格。最著名的產區之一是法國的卡多內（Loire）地區，特別是來自蘋果卡多內（Sancerre）和普依-芒塔布（Pouilly-Fumé）的白蘇維濃葡萄酒。此外，紐西蘭（特別是馬爾伯勒地區）、美國（尤其是加州和華盛頓州）、智利和南非也生產優質的白蘇維濃葡萄酒。
+              配餐建議：白蘇維濃葡萄酒搭配海鮮、沙拉、白肉、蔬菜和各種乳酪非常出色。它們的酸度和清新口感使其成為許多不同風味的菜餚的絕佳伴侶。`},{name:"夏多麗",image:"/images/knowledge/chardonnay.png",description:`起源：夏多麗葡萄品種最早起源於法國，特別是勃艮第地區。它是由希瓦（Pinot）和貝爾布蘭克（Gouais
+              Blanc）兩個葡萄品種的自然雜交所形成。夏多麗葡萄在全球各地廣泛種植，成為世界上最重要的白葡萄品種之一。
+              葡萄特性：夏多麗葡萄通常呈現中等大小的葡萄粒，皮厚。這種葡萄品種具有高酸度，這使得它成熟後保持清新和活潑的風味，同時也易於釀造不同風格的白葡萄酒。
+              風味特點：夏多麗葡萄酒的風味特點因產地和釀造方法的不同而變化很大。它們通常呈現綠蘋果、柚子、梨子、柑橘、菠蘿等水果風味，並伴隨著奶油、香草、堅果和礦物質的氣息。夏多麗葡萄酒可以是清新的、酸度高的，也可以是富有濃度、圓潤和帶有橡木桶風味的複雜型白葡萄酒。
+              葡萄酒產區：夏多麗葡萄品種在全球各地都有廣泛種植，並且被廣泛用於各種白葡萄酒的釀造。法國的勃艮第地區、勃艮第的莫爾東、香檳、波爾多地區的白海岸（白貝爾格）以及新世界的加州、澳大利亞、紐西蘭、南非等地都以生產出色的夏多麗葡萄酒而聞名。
+              配餐建議：夏多麗葡萄酒非常適合搭配各種食物，包括家禽、魚類、海鮮、蔬菜、乳酪和各種沙拉。它的風味多樣性和酸度使其成為許多菜餚的理想伴侶。`}],openers:[{title:"螺旋鑽式開瓶器（侍酒師之友）",description:`將開瓶器的尖端對準軟木塞的中心。<br>
+              旋轉手柄，將螺旋鑽緩慢旋入軟木塞中。<br>
+              當螺旋鑽幾乎完全進入軟木塞時停止。<br>
+              使用開瓶器的杠杆部分抵住瓶頸邊緣，輕輕拉起手柄，將軟木塞從瓶子中取出。`,image:"/images/knowledge/opener1.png",isOpen:!1},{title:"翼式開瓶器",description:`將開瓶器的中心針對准軟木塞中心。<br>
+              旋轉頂部的手柄，使螺旋鑽進入軟木塞。<br>
+              繼續旋轉直到兩邊的“翼”完全上升。<br>
+              按下兩邊的“翼”，以此力量將軟木塞從瓶中拔出。`,image:"/images/knowledge/opener2.png",isOpen:!1},{title:"電動開瓶器",description:`將電動開瓶器的開口對準軟木塞。<br>
+              啟動電動機，讓其自動將螺旋鑽旋入軟木塞中。<br>
+              當開瓶器停止旋轉時，說明軟木塞已被完全拔出。<br>
+              可能需要按下另一個按鈕來釋放軟木塞。`,image:"/images/knowledge/opener3.png",isOpen:!1}],decantingTechniques:[{title:"使用醒酒器（Decanter）",description:`<strong>操作方法：</strong>將葡萄酒緩緩倒入醒酒器中。醒酒器通常有寬闊的底部，可以增加葡萄酒與空氣的接觸面積。<br>
+          <strong>適用類型：</strong>適用於年份較老的紅葡萄酒，或者需要釋放更多香氣和風味的年輕、單寧較重的葡萄酒。`,image:"/images/knowledge/glass1.png",isOpen:!1},{title:"直接在酒杯中醒酒",description:`<strong>操作方法：</strong>將葡萄酒倒入寬口酒杯中，讓它在杯中搖晃幾分鐘。<br>
+          <strong>適用類型：</strong>適用於不需要長時間醒酒的葡萄酒，或當你沒有醒酒器時的臨時解決方案。`,image:"/images/knowledge/glass2.png",isOpen:!1},{title:"旋轉酒杯",description:`<strong>操作方法：</strong>倒入少量葡萄酒後，輕輕旋轉酒杯，讓葡萄酒沿杯壁轉動，這樣可以增加葡萄酒與空氣的接觸。<br>
+          <strong>適用類型：</strong>幾乎適用於所有類型的葡萄酒，特別是在品酒時。`,image:"/images/knowledge/glass3.png",isOpen:!1},{title:"使用醒酒塞或醒酒漏斗",description:`<strong>操作方法：</strong>這些工具可以在倒酒時幫助葡萄酒與空氣混合。醒酒塞或漏斗安裝在瓶口，當酒倒出時自然引入空氣。<br>
+          <strong>適用類型：</strong>適合需要稍微醒酒的年輕紅葡萄酒和某些白葡萄酒。`,image:"/images/knowledge/glass4.png",isOpen:!1},{title:"提前開瓶",description:`<strong>操作方法：</strong>提前幾小時打開葡萄酒瓶，讓酒瓶口與空氣接觸。<br>
+          <strong>適用類型：</strong>適用於大多數紅葡萄酒，尤其是單寧較重的品種。`,image:"/images/knowledge/glass5.png",isOpen:!1}]}},methods:{toggleOpener(n){this.openers[n].isOpen=!this.openers[n].isOpen},toggleTechnique(n){this.decantingTechniques[n].isOpen=!this.decantingTechniques[n].isOpen}}},r=n=>(h("data-v-5e065777"),n=n(),v(),n),w={class:"container"},k={class:"container"},f=r(()=>e("div",{class:"row justify-content-center"},[e("h2",{class:"col-10 text-primary text-start my-4"},"紅葡萄品種")],-1)),y={class:"row"},O={class:"row list-unstyled"},S={class:"row"},T=["src"],B=["innerHTML"],C={class:"container mt-5"},M=r(()=>e("h2",null,"開瓶器使用方式",-1)),G=r(()=>e("p",null,"瞭解不同類型的開瓶器及其使用方法。",-1)),L=["onClick"],P={class:"mb-0"},V={class:"btn btn-link",type:"button"},$={key:0,class:"collapse show"},j={class:"row"},H=["innerHTML"],q=["src"],F={class:"container mt-5"},I=r(()=>e("h2",{class:"mb-4"},"醒酒技巧",-1)),N=r(()=>e("p",null,"醒酒是指讓葡萄酒與空氣接觸，促進其釋放香氣和風味的過程。",-1)),D=["onClick"],K={class:"accordion-header"},z={class:"accordion-button",type:"button"},A={key:0,class:"collapse show"},E={class:"row"},R=["innerHTML"],J=["src"];function Q(n,U,W,X,c,m){return t(),o(l,null,[e("div",w,[e("div",k,[f,e("div",y,[e("ol",O,[(t(!0),o(l,null,d(c.redGrapeVarieties,(i,s)=>(t(),o("li",{class:"col-12",key:`variety-${s}`},[e("div",S,[e("div",{class:a({"col-md-4 order-md-last":s%2===0,"col-md-4":s%2!==0})},[e("img",{src:n.$filters.imgPath(i.image),class:"img-fluid mb-4 rounded-3",alt:"variety"},null,8,T)],2),e("div",{class:a({"col-md-8 order-md-first":s%2===0,"col-md-8":s%2!==0})},[e("h3",null,g(i.name),1),e("p",{innerHTML:i.description},null,8,B)],2)])]))),128))])])]),e("div",C,[M,G,(t(!0),o(l,null,d(c.openers,(i,s)=>(t(),o("div",{key:`opener-${s}`,class:"card"},[e("div",{class:"card-header",onClick:_=>m.toggleOpener(s)},[e("h5",P,[e("button",V,g(i.title),1)])],8,L),i.isOpen?(t(),o("div",$,[e("div",j,[e("div",{class:a({"col-md-8 order-md-first":s%2===0,"col-md-8":s%2!==0})},[e("div",{class:"card-body",innerHTML:i.description},null,8,H)],2),e("div",{class:a({"col-md-4 order-md-last":s%2===0,"col-md-4":s%2!==0})},[e("img",{src:n.$filters.imgPath(i.image),class:"w-100 mb-5",alt:"info"},null,8,q)],2)])])):p("",!0)]))),128))])]),e("div",F,[I,N,(t(!0),o(l,null,d(c.decantingTechniques,(i,s)=>(t(),o("div",{key:`technique-${s}`,class:"card"},[e("div",{class:"card-header",onClick:_=>m.toggleTechnique(s)},[e("h2",K,[e("button",z,g(i.title),1)])],8,D),i.isOpen?(t(),o("div",A,[e("div",E,[e("div",{class:"card-body",innerHTML:i.description},null,8,R)]),e("div",{class:a({"col-md-4 order-md-last":s%2===0,"col-md-4":s%2!==0})},[e("img",{src:n.$filters.imgPath(i.image),class:"w-100 mb-5",alt:"info"},null,8,J)],2)])):p("",!0)]))),128))])],64)}const Z=u(b,[["render",Q],["__scopeId","data-v-5e065777"]]);export{Z as default};
---
+++ assets/wine hand style-CA-_iX33.js
@@ -0,0 +1,1 @@
+const e="/Wine-project/images/wine hand style.png";export{e as _};
