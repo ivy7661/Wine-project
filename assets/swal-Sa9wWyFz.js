dfcaf6dfad44bb3487aba3447a261aca0d8e0eb3
Ray Lin <ray102467@gmail.com>
2024-04-09 11:15:26 +0800

    Updates

--- assets/swal-Sa9wWyFz.js
+++
@@ -1,1 +0,0 @@
-import{S as r}from"./sweetalert2.all-edPz5k6j.js";const e={toastTop(t,o){return r.fire({icon:t,title:o,toast:!0,position:"top",showConfirmButton:!1,timer:1500,width:280})}};export{e as A};
